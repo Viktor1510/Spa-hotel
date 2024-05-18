c42d521274df232b98f296f3dbd1bbe17759ca02
Kristiyan <78322632+whazxc@users.noreply.github.com>
2024-05-18 18:32:46 +0300

    Update functions.js
    затваряне/отваряне на логин

--- SignIn/javascript/functions.js
+++ SignIn/javascript/functions.js
@@ -1,17 +1,26 @@
 let signInBtn = document.getElementById("signInBtn");
 let signUpBtn = document.getElementById("signUpBtn");
-let emailInput = document.getElementById("emailInput");
+let closeOverlayBtn = document.getElementById("closeBtn");
+let overlayLogin = document.getElementById("overlayLogin");
+let overlayRegister = document.getElementById("overlayRegister");
 
-// праща user-a в менюто за "регистрация"
-signUpBtn.onclick = function(){
-    signInBtn.classList.add("disable");
-    signUpBtn.classList.remove("disable");
-    window.location.href = "../html/signup.html";
-}
-
-// праща user-a в менюто за "вход"
-signInBtn.onclick = function(){
+function triggerOverlaySignIn(event){
     signInBtn.classList.remove("disable");
     signUpBtn.classList.add("disable");
-    window.location.href = "../html/signin.html";
+    document.getElementById("overlaySignUp").style.display = "none";
+    document.getElementById("overlaySignIn").style.display = "block";
+    event.preventDefault();
+}
+function triggerOverlaySignUp(event){
+    signInBtn.classList.add("disable");
+    signUpBtn.classList.remove("disable");
+    document.getElementById("overlaySignIn").style.display = "none";
+    document.getElementById("overlaySignUp").style.display = "block";
+    event.preventDefault();
+}
+function closeOverlaySignIn(){
+    document.getElementById("overlaySignIn").style.display = "none";
+}
+function closeOverlaySignUp(){
+    document.getElementById("overlaySignUp").style.display = "none";
 }
